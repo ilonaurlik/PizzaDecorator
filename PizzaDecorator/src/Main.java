9c606594e555f9d5aecb4141929d2267d1d06ff7
ilonaurlik <37349626+ilonaurlik@users.noreply.github.com>
2018-08-23 01:49:39 +0200

    Javadoc

--- PizzaDecorator/src/Main.java
+++ PizzaDecorator/src/Main.java
@@ -1,3 +1,9 @@
+/**
+ * The program allows to add new ingredients(Ham and Cheese) to object Pizza by using decorator pattern. 
+ * It calculates new price after adding one or two ingredients. The program adds the names and price of ingredients to 
+ * the name of the Pizza.
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
